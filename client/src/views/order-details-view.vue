b03d0ebfcaed81f1dca87120a2f86ebf26b8ddb9
LucasGitB <77726940+LucasGitB@users.noreply.github.com>
2024-06-13 20:08:20 +0200

    Iw1 s2 g21 69 - page des commandes  (#5)
    * Création page et element d'une commande
    
    * Ajout du statut de la commande
    
    * Ajout de la page des details de la commande
    
    * Page des details d'une commnande
    
    * Fix : page des details d'une commnande
    
    * Page des commandes et details de la commande
    
    * Fix css
    
    * Fix css
    
    * Fix css

---
+++ client/src/views/order-details-view.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { MoveLeft } from 'lucide-vue-next';
+import Order from '@/components/order.vue';
+</script>
+
+<template>
+  <div class="flex items-center w-full relative align">
+    <div class="block lg:hidden">
+      <RouterLink :to="{ name: 'orders' }"><MoveLeft /></RouterLink>
+    </div>
+
+    <h1 class="font-bold text-lg text-center w-full sm:text-3xl">Détails de la commande</h1>
+  </div>
+  <Order />
+</template>
---
+++ client/src/views/order-view.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import OrderList from '@/components/order-list.vue';
+
+
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold m-2">Mes commandes</h1>
+  <div><OrderList/></div>
+</template>
