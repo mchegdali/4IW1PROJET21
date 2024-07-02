12db97686823b02628c6da8b42eb172592860035
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-07-03 00:53:07 +0200

    test deploy on vps

--- server/models/sql/db.js
+++ server/models/sql/db.js
@@ -1,6 +1,12 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL);
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+});
 
 sequelize.authenticate().then(() => {
   console.log('[SQL] Connected to database');
