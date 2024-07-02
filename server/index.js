068c0b78e39355fd16344d7f44360cc5b523dafc
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-07-02 23:57:21 +0200

    test deploy on vps

--- server/index.js
+++ server/index.js
@@ -5,6 +5,6 @@ const zodErrorMap = require('./utils/zod-error-map');
 // Initialize translated error messages
 z.setErrorMap(zodErrorMap);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
 });
