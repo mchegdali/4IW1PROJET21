6df62d91da2334a7903fd9f3a096d4621390b134
LucasGitB <lucasaguesse@gmail.com>
2024-07-03 20:01:44 +0200

    Acceder à  ses adresses

---
+++ client/src/views/adresse-view.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { MoveLeft } from 'lucide-vue-next';
+</script>
+
+<template>
+  <div class="flex items-center w-full relative align">
+    <div class="block lg:hidden">
+      <RouterLink :to="{ name: 'account' }"><MoveLeft /></RouterLink>
+    </div>
+
+    <h1 class="font-bold text-lg text-center w-full sm:text-3xl">Carnet d’adresses</h1>
+  </div>
+
+  <div class="mt-10 flex flex-col gap-10">
+    <Button type="submit" class="w-full bg-tea-600 rounded-lg text-white p-2"
+      >Ajouter une nouvelle adresse</Button
+    >
+
+    <div class="rounded shadow-md p-2 flex justify-between">
+      <div>
+        <p>Jean Dupont</p>
+        <p>52 rue Antoine</p>
+        <p>Paris</p>
+        <p>Ile de France</p>
+        <p>77016</p>
+        <p>France</p>
+        <p>07 45 62 41 23</p>
+      </div>
+
+      <div>Modifier</div>
+    </div>
+  </div>
+</template>
