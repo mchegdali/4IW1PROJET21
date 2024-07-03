e513220b69e6940c27710ee95a228f72081487f3
LucasGitB <lucasaguesse@gmail.com>
2024-07-03 20:00:49 +0200

    Modifier son profil

---
+++ client/src/components/my-information-form.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { reactive, computed } from 'vue';
+import Input from './ui/input/Input.vue';
+import Button from '@/components/ui/button/Button.vue';
+import { z } from 'zod';
+import { useForm } from '@/composables/form';
+
+const loginSchema = z.object({
+  email: z.string().email({ message: "L'e-mail doit être sous cette forme : exemple@example.com" }),
+  name: z.string().regex(/^[a-zA-ZÀ-ÿ' -]+ [a-zA-ZÀ-ÿ' -]+$/, {
+    message: 'Ne peut contenir que des lettres, des apostrophes, des tirets et des accents.'
+  })
+});
+
+const initialLoginData = reactive({
+  email: 'eokfiejf@fefrf.com',
+  name: 'toto'
+});
+
+const { formData, formErrors, formSubmitting, submitForm } = useForm(loginSchema, initialLoginData);
+
+const handleSubmit = () => {
+  submitForm(async (data) => {
+    console.log('Données de connexion envoyées :', data);
+  });
+};
+
+const isFormModified = computed(() => {
+  return formData.email !== initialLoginData.email || formData.name !== initialLoginData.name;
+});
+</script>
+
+<template>
+  <div>
+    <form class="flex flex-col gap-4" @submit.prevent="handleSubmit">
+      <div>
+        <label for="name">Nom et Prénom</label>
+        <Input
+          id="name"
+          type="text"
+          v-model="formData.name"
+          :class="{ 'border-destructive': formErrors.name }"
+        />
+        <small class="text-destructive" v-if="formErrors.name">{{ formErrors.name }}</small>
+      </div>
+      <div>
+        <label for="email">Adresse e-mail</label>
+        <Input
+          id="email"
+          type="email"
+          v-model="formData.email"
+          autofocus
+          :class="{ 'border-destructive': formErrors.email }"
+        />
+        <small class="text-destructive" v-if="formErrors.email">{{ formErrors.email }}</small>
+      </div>
+      <Button type="submit" class="w-full" :disabled="formSubmitting || !isFormModified">Valider les modifications</Button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.text-destructive {
+  color: red;
+}
+.border-destructive {
+  border: 1px solid red;
+}
+</style>
