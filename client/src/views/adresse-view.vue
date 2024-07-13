429b77ecc919e2403a48ae0664e0c60583f67c09
LucasGitB <lucasaguesse@gmail.com>
2024-07-13 19:09:14 +0200

    front get addresses and delete

--- client/src/views/adresse-view.vue
+++ client/src/views/adresse-view.vue
@@ -1,5 +1,43 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
 import { MoveLeft } from 'lucide-vue-next';
+
+const user = JSON.parse(localStorage.getItem('user') || '{}');
+const userId = user.id;
+
+const addresses = ref([]);
+
+const getAddresses = async () => {
+  try {
+    const response = await fetch(`http://localhost:3000/users/${userId}/addresses/`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    addresses.value = data;
+  } catch (error) {
+    console.error('Erreur lors de la récupération des adresses:', error);
+  }
+};
+
+const deleteAddress = async (addressId) => {
+  try {
+    const response = await fetch(`http://localhost:3000/users/${userId}/addresses/${addressId}`, {
+      method: 'DELETE'
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    addresses.value = addresses.value.filter((address) => address._id !== addressId);
+  } catch (error) {
+    console.error("Erreur lors de la suppression de l'adresse:", error);
+  }
+};
+
+onMounted(() => {
+  getAddresses();
+});
 </script>
 
 <template>
@@ -12,22 +50,30 @@ import { MoveLeft } from 'lucide-vue-next';
   </div>
 
   <div class="mt-10 flex flex-col gap-10">
-    <RouterLink :to="{ name: 'add-adresse' }" class="w-full bg-tea-600 rounded-lg text-white p-2 text-center">Ajouter une nouvelle adresse</RouterLink>
-  
+    <RouterLink
+      :to="{ name: 'add-adresse' }"
+      class="w-full bg-tea-600 rounded-lg text-white p-2 text-center"
+      >Ajouter une nouvelle adresse</RouterLink
+    >
 
-    <div class="rounded shadow-md p-2 flex justify-between">
+    <div
+      v-for="address in addresses"
+      :key="address.id"
+      class="rounded shadow-md p-2 flex justify-between"
+    >
       <div>
-        <p>Jean Dupont</p>
-        <p>52 rue Antoine</p>
-        <p>Paris</p>
-        <p>Ile de France</p>
-        <p>77016</p>
-        <p>France</p>
-        <p>07 45 62 41 23</p>
+        <p>{{ address.name }}</p>
+        <p>{{ address.street }}</p>
+        <p>{{ address.city }}</p>
+        <p>{{ address.zipCode }}</p>
       </div>
 
-      <RouterLink :to="{ name: 'edit-adresse' }" class="">Modifier</RouterLink>
-  
+      <div class="flex gap-2">
+        <RouterLink :to="{ name: 'edit-adresse', params: { id: address.id } }" class=""
+          >Modifier</RouterLink
+        >
+        <button @click="deleteAddress(address._id)" class="text-red-500">Supprimer</button>
+      </div>
     </div>
   </div>
 </template>
