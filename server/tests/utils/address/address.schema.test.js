7aab846301aeaddf1bdfaaa32e6185a7947a77c8
LucasGitB <lucasaguesse@gmail.com>
2024-07-16 20:44:52 +0200

    Test du schema pour une adresse invalide/

--- server/tests/utils/address/address.schema.test.js
+++ server/tests/utils/address/address.schema.test.js
@@ -16,4 +16,32 @@ describe('Address Schema Validation', () => {
     const validation = addressCreateSchema.safeParse(addressValid);
     expect(validation.success).toBe(true);
   });
+
+  it('should fail validation for incorrect address data', () => {
+    const addressInvalid = {
+      firstName: 'J',
+      lastName: 'D',
+      street: '1',
+      city: 'P4ris',
+      region: 'Îl3-de-Fr4nce',
+      zipCode: '7500',
+      country: 'Fr4nce',
+      phone: '012345678',
+    };
+
+    const validation = addressCreateSchema.safeParse(addressInvalid);
+    expect(validation.success).toBe(false);
+    expect(validation.error.errors).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ message: 'Prénom invalide' }),
+        expect.objectContaining({ message: 'Nom de famille invalide' }),
+        expect.objectContaining({ message: 'Adresse invalide.' }),
+        expect.objectContaining({ message: 'Ville invalide' }),
+        expect.objectContaining({ message: 'Région invalide' }),
+        expect.objectContaining({ message: 'Code postal invalide' }),
+        expect.objectContaining({ message: 'Pays invalide' }),
+        expect.objectContaining({ message: 'Numéro de téléphone invalide' }),
+      ]),
+    );
+  });
 });
