93f4d24427852ac918417e67654501d6c4361840
37anton <antonin.cavanne@outlook.fr>
2024-07-16 21:57:13 +0200

    Feat: ajout du back pour line chart dans /client dashboard Admin

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -284,6 +284,32 @@ async function getUserCount(req, res, next) {
   }
 }
 
+/**
+ * Récupérer le nombre d'inscriptions d'utilisateurs par jour sur Mongo
+ *
+ * @type {import('express').RequestHandler}
+ * @returns
+ */
+async function getUserRegistrations(req, res, next) {
+  try {
+      const registrations = await UserMongo.aggregate([
+          {
+              $group: {
+                  _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+                  count: { $sum: 1 }
+              }
+          },
+          { $sort: { _id: 1 } }
+      ]);
+      return res.status(200).json(registrations.map(entry => ({
+          date: entry._id,
+          count: entry.count
+      })));
+  } catch (error) {
+      return next(error);
+  }
+}
+
 module.exports = {
   getUserCount,
   getUserCount,
