68bd6165ca183aff676dd2880bf417eb9bec6c91
37anton <antonin.cavanne@outlook.fr>
2024-07-16 21:57:13 +0200

    Feat: ajout du back pour line chart dans /client dashboard Admin

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -14,6 +14,7 @@ const { NotFound } = require('http-errors');
 const Users = sequelize.model('users');
 
 /**
+ * Créer un utilisateur
  *
  * @type {import('express').RequestHandler}
  * @returns
@@ -282,6 +283,32 @@ async function getUserCount(req, res, next) {
   }
 }
 
+/**
+ * Récupérer le nombre d'inscriptions d'utilisateurs par jour sur Mongo
+ *
+ * @type {import('express').RequestHandler}
+ * @returns
+ */
+async function getUserRegistrations(req, res, next) {
+  try {
+      const registrations = await UserMongo.aggregate([
+          {
+              $group: {
+                  _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+                  count: { $sum: 1 }
+              }
+          },
+          { $sort: { _id: 1 } }
+      ]);
+      return res.status(200).json(registrations.map(entry => ({
+          date: entry._id,
+          count: entry.count
+      })));
+  } catch (error) {
+      return next(error);
+  }
+}
+
 module.exports = {
   getUserCount,
   createUser,
@@ -290,5 +317,5 @@ module.exports = {
   replaceUser,
   deleteUser,
   updateUser,
-  getUserAddresses,
+  getUserRegistrations,
 };
