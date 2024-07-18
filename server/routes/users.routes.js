e2492b2f5317b11fe158cd90f307f84d5ee9a11b
37anton <antonin.cavanne@outlook.fr>
2024-07-18 14:44:16 +0200

    Reorganisation des routes user car les routes statiques bugaient à cause des routes dynamiques placées après

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -31,6 +31,22 @@ usersRouter.get(
   getUsers,
 );
 
+// Route pour obtenir le nombre total d'utilisateurs
+usersRouter.get(
+  '/users/count',
+  checkAuth(authConfig.accessTokenSecret, false),
+  checkRole(['admin']),
+  getUserCount,
+);
+
+// Route pour obtenir le nombre d'inscriptions d'utilisateurs par jour
+usersRouter.get(
+  '/users/registrations',
+  checkAuth(authConfig.accessTokenSecret, false),
+  checkRole(['admin']),
+  getUserRegistrations,
+);
+
 usersRouter.use('/users/:userId/addresses', addressesRouter);
 usersRouter.get('/users/:userId', getUser);
 
