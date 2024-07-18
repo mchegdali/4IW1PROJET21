8e155d76310b353105de57cb24803f348ccd7e69
37anton <antonin.cavanne@outlook.fr>
2024-07-18 22:28:49 +0200

    controller, schema, route pour Status + maj de app.js

---
+++ server/routes/status.routes.js
@@ -0,0 +1,51 @@
+const { Router } = require('express');
+const {
+  createStatus,
+  getStatuses,
+  replaceStatus,
+  deleteStatus,
+  updateStatus,
+} = require('../controllers/status.controller');
+const { checkAuth, checkRole } = require('../middlewares/auth.middleware');
+const authConfig = require('../config/auth.config');
+
+const statusRouter = Router();
+
+// Route pour créer un status
+statusRouter.post(
+  '/status',
+  // checkAuth(authConfig.accessTokenSecret, true),
+  createStatus,
+);
+
+// Route pour obtenir tous les statuts
+statusRouter.get(
+  '/status',
+  checkAuth(authConfig.accessTokenSecret, false),
+  checkRole(['admin']),
+  getStatuses,
+);
+
+statusRouter.get('/status/:id', getStatus);
+
+statusRouter.put(
+  '/status/:id',
+  checkAuth(authConfig.accessTokenSecret, true),
+  replaceStatus,
+);
+
+statusRouter.patch(
+  '/status/:id',
+  checkAuth(authConfig.accessTokenSecret, true),
+  updateStatus,
+);
+
+// Route pour supprimer un status
+statusRouter.delete(
+  '/status/:id',
+  checkAuth(authConfig.accessTokenSecret, true),
+  checkRole(['admin']),
+  deleteStatus,
+);
+
+module.exports = statusRouter;
---
+++ server/schemas/status.schema.js
@@ -0,0 +1,32 @@
+const { z } = require('zod');
+const entitySchema = require('./entity.schema');
+const timestampsSchema = require('./timestamps.schema');
+const { paginationSchema } = require('./pagination.schema');
+
+const statusQuerySchema = z
+  .object({
+    text: z.string().min(2).optional(),
+  })
+  .merge(paginationSchema);
+
+const statusCreateSchema = z.object({
+  label: z.string().min(1, 'Libellé obligatoire').trim(),
+});
+
+const statusUpdateSchema = statusCreateSchema.partial().refine((a) => {
+  return Object.keys(a).length > 0;
+}, 'Vous devez spécifier au moins un champ');
+
+const statusSchema = statusCreateSchema.merge(entitySchema).merge(timestampsSchema);
+
+/** @typedef { z.infer<typeof statusCreateSchema>} StatusCreateBody */
+/** @typedef { z.infer<typeof statusUpdateSchema>} StatusUpdateBody */
+/** @typedef { z.infer<typeof statusSchema>} Status */
+/** @typedef { z.infer<typeof statusQuerySchema>} StatusQuery */
+
+module.exports = {
+  statusCreateSchema,
+  statusUpdateSchema,
+  statusSchema,
+  statusQuerySchema,
+};
