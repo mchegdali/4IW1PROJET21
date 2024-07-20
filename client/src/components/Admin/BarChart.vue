02ac17bb41e4c0e627ffea9f41f5f7acfe141b62
37anton <antonin.cavanne@outlook.fr>
2024-07-20 18:15:40 +0200

    récupération des inscriptions des 7 derniers mois pour le barchart dans admin/home

--- client/src/components/Admin/BarChart.vue
+++ client/src/components/Admin/BarChart.vue
@@ -1,26 +1,30 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref, defineProps } from 'vue';
 
-const options = ref({
-    chart: {
-        id: 'vuechart-example'
-    },
-    xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-    }
-});
+interface BarChartOptions {
+  chart: {
+    id: string;
+  };
+  xaxis: {
+    categories: string[];
+  };
+}
 
-const series = ref<Array<{ name: string; data: number[] }>>([{
-    name: 'series-1',
-    data: [30, 40, 45, 50, 49, 60, 70, 91]
-}]);
+interface BarChartSeries {
+  name: string;
+  data: number[];
+}
+
+const props = defineProps<{ options: BarChartOptions; series: BarChartSeries[] }>();
+
+const options = ref(props.options);
+const series = ref(props.series);
 </script>
 
 <template>
-    <div class="w-full">
-        <apexchart width="100%" type="bar" :options="options" :series="series"></apexchart>
-    </div>
+  <div class="w-full">
+    <apexchart width="100%" type="bar" :options="options" :series="series"></apexchart>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
