32958b97d66a0c5b8972514eff0b946ad646f8be
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-07-20 18:04:32 +0200

    fix useform

--- client/src/components/my-information-form.vue
+++ client/src/components/my-information-form.vue
@@ -5,61 +5,54 @@ import Button from '@/components/ui/button/Button.vue';
 import { z } from 'zod';
 import { useForm } from '@/composables/form';
 
-const loginSchema = z.object({
+const userInfosSchema = z.object({
   email: z.string().email({ message: "L'e-mail doit être sous cette forme : exemple@example.com" }),
   name: z.string().regex(/^[a-zA-ZÀ-ÿ' -]+ [a-zA-ZÀ-ÿ' -]+$/, {
     message: 'Ne peut contenir que des lettres, des apostrophes, des tirets et des accents.'
   })
 });
 
-const initialLoginData = reactive({
-  email: '',
-  name: ''
-});
-
-const { formData, formErrors, formSubmitting, submitForm } = useForm(loginSchema, initialLoginData);
-
-const handleSubmit = () => {
-  submitForm(async (data) => {
-    console.log('Données de connexion envoyées :', data);
+const { handleSubmit, isSubmitting, isError, defineField, errors, cancel, status, formValues } =
+  useForm({
+    validationSchema: userInfosSchema,
+    defaultValues: {
+      email: '',
+      name: ''
+    }
   });
-};
+
+const [email, emailField] = defineField('email');
+const [name, nameField] = defineField('name');
+
+const submitHandler = handleSubmit(async (data, signal) => {});
 </script>
 
 <template>
   <div>
-    <form class="flex flex-col gap-4" @submit.prevent="handleSubmit">
+    <form class="flex flex-col gap-4" @submit.prevent="submitHandler">
       <div>
         <label for="name">Nom et Prénom</label>
         <Input
           id="name"
           type="text"
-          v-model="formData.name"
-          :class="{ 'border-destructive': formErrors.name }"
+          v-model="name"
+          @input="nameField.onInput"
+          :class="{ 'border-destructive': errors.name }"
         />
-        <small class="text-destructive" v-if="formErrors.name">{{ formErrors.name }}</small>
+        <small class="text-destructive" v-if="errors.name">{{ errors.name }}</small>
       </div>
       <div>
         <label for="email">Adresse e-mail</label>
         <Input
           id="email"
           type="email"
-          v-model="formData.email"
-          autofocus
-          :class="{ 'border-destructive': formErrors.email }"
+          v-model="email"
+          @input="emailField.onInput"
+          :class="{ 'border-destructive': errors.email }"
         />
-        <small class="text-destructive" v-if="formErrors.email">{{ formErrors.email }}</small>
+        <small class="text-destructive" v-if="errors.email">{{ errors.email }}</small>
       </div>
       <Button type="submit" class="w-full">Valider les modifications</Button>
     </form>
   </div>
 </template>
-
-<style scoped>
-.text-destructive {
-  color: red;
-}
-.border-destructive {
-  border: 1px solid red;
-}
-</style>
