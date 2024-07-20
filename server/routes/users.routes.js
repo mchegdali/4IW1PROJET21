309456734b035cd394e88fa0a9a03d72ddd2ad54
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-07-20 23:06:01 +0200

    fix middleware in getUser

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -8,7 +8,7 @@ const {
   getUser,
   getUserCount,
   getUserRegistrations,
-  getUserRegistrationsLast12Months
+  getUserRegistrationsLast12Months,
 } = require('../controllers/users.controller');
 const { checkAuth, checkRole } = require('../middlewares/auth.middleware');
 const authConfig = require('../config/auth.config');
@@ -34,7 +34,6 @@ usersRouter.get(
   getUserRegistrationsLast12Months,
 );
 
-
 // Route pour obtenir le nombre d'inscriptions d'utilisateurs par jour
 usersRouter.get(
   '/users/registrations',
@@ -59,7 +58,7 @@ usersRouter.use(
 
 usersRouter.get(
   '/users/:userId',
-  checkAuth(authConfig.accessTokenSecret, true),
+  checkAuth(authConfig.accessTokenSecret, false),
   isOwnAccount,
   getUser,
 );
