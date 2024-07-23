5cd8581dc76f12e64fe15d3464d9824e69e72bff
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-07-24 01:05:32 +0200

    fix cors

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const logger = require('pino-http');
+// const logger = require('pino-http');
 const helmet = require('helmet');
 const path = require('path'); // Ajouté pour le chemin du dossier
 
-const corsOptions = {
-  origin: process.env.APP_URL,
-  optionsSuccessStatus: 200,
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: process.env.APP_URL,
+//   optionsSuccessStatus: 200,
+//   credentials: true,
+// };
 
 const authRouter = require('./routes/auth.routes');
 const productsRouter = require('./routes/products.routes');
@@ -24,44 +24,17 @@ const app = express();
 app.set('trust proxy', 1);
 
 app.use(express.json());
-// app.use(cors()); // Middleware CORS global pour toutes les routes
+app.use(cors()); // Middleware CORS global pour toutes les routes
 app.use(
   helmet({
     crossOriginResourcePolicy: false, // Désactiver la politique de ressource cross-origin
   }),
 );
 
-// Configurer les en-têtes CORS pour toutes les réponses
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//   if (req.method === 'OPTIONS') {
-//     res.sendStatus(200);
-//   } else {
-//     next();
-//   }
-// });
-
 // Utiliser le middleware CORS uniquement pour les routes de fichiers statiques
 const staticFileMiddleware = express.static(path.join(__dirname, 'uploads'));
 
-app.use(
-  '/uploads',
-  cors(),
-  (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-    );
-    next();
-  },
-  staticFileMiddleware,
-);
-app.use(cors(corsOptions));
-// app.use(logger());
+app.use('/uploads', staticFileMiddleware);
 app.use(helmet());
 
 app.use(authRouter);
