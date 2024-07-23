b8d0b94134e120ad3a34412fba22071d1a2e81b0
LucasGitB <lucasaguesse@gmail.com>
2024-07-23 15:03:44 +0200

    fix isOwner middleware

--- server/middlewares/order.middleware.js
+++ server/middlewares/order.middleware.js
@@ -1,15 +1,24 @@
 const OrderMongo = require('../models/mongo/orders.mongo');
+const uuidSchema = require('../schemas/uuid.schema');
 
 /**
  *
- * @type {import("express").RequestHandler}
- */
+ 
+@type {import("express").RequestHandler}*/
 const isOwnOrder = async (req, res, next) => {
-  const order = await OrderMongo.findById(req.params.id);
+  const { data: id, success } = uuidSchema.safeParse(req.params.id);
+
+  if (!success) {
+    return res.sendStatus(404);
+  }
+
+  const order = await OrderMongo.findById(id);
   if (!order) {
     return res.sendStatus(404);
   }
-  const isAuthorized = req.user._id.toString() === order.user._id.toString() || req.user.role === 'admin';
+  const isAuthorized =
+    req.user._id.toString() === order.user._id.toString() ||
+    req.user.role === 'admin';
   if (!isAuthorized) {
     return res.sendStatus(403);
   }
