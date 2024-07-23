c790ba3a37df46f5506edec20537cd754f31123c
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-07-23 22:43:24 +0200

    create upload folder on startup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
+const fs = require('node:fs');
 const { z } = require('zod');
 const app = require('./app');
 const zodErrorMap = require('./utils/zod-error-map');
 
 // Initialize translated error messages
 z.setErrorMap(zodErrorMap);
+
+try {
+  fs.mkdirSync('./uploads');
+} catch {
+  // Do nothing
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
